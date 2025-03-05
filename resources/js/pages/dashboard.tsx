aa90a84077e996fd193f29ac4021d56a876ef1e9
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2025-03-06 06:15:39 +0800

    style: setup dashboard

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,7 +1,7 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { FolderTree, MessageCircleQuestion, Send, Tag } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,24 +10,46 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Dashboard() {
+export default function Dashboard({ posts }: { posts: number }) {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+            <div className="grid grid-cols-2 gap-4 rounded-xl p-4 xl:grid-cols-4">
+                <div className="border-xl w-full rounded-lg border px-7 py-6 shadow">
+                    <div className="flex items-center justify-between py-2">
+                        <h6 className="text-sm font-semibold">Total Postingan</h6>
+                        <Send className="size-4" />
                     </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    <div className="block py-2">
+                        <h3 className="text-4xl font-extrabold">{posts}</h3>
                     </div>
                 </div>
-                <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                <div className="border-xl w-full rounded-lg border px-7 py-6 shadow">
+                    <div className="flex items-center justify-between py-2">
+                        <h6 className="text-sm font-semibold">Total Tag</h6>
+                        <Tag className="size-4" />
+                    </div>
+                    <div className="block py-2">
+                        <h3 className="text-4xl font-extrabold">78</h3>
+                    </div>
+                </div>
+                <div className="border-xl w-full rounded-lg border px-7 py-6 shadow">
+                    <div className="flex items-center justify-between py-2">
+                        <h6 className="text-sm font-semibold">Total Kategori</h6>
+                        <FolderTree className="size-4" />
+                    </div>
+                    <div className="block py-2">
+                        <h3 className="text-4xl font-extrabold">80</h3>
+                    </div>
+                </div>
+                <div className="border-xl w-full rounded-lg border px-7 py-6 shadow">
+                    <div className="flex items-center justify-between py-2">
+                        <h6 className="text-sm font-semibold">Total Pertanyaan</h6>
+                        <MessageCircleQuestion className="size-4" />
+                    </div>
+                    <div className="block py-2">
+                        <h3 className="text-4xl font-extrabold">100</h3>
+                    </div>
                 </div>
             </div>
         </AppLayout>
