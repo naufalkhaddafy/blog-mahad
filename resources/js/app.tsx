bf5da42927ee1a5af385c7633e634b4a0c6ff99d
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2025-03-06 06:12:27 +0800

    set: app name

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -10,11 +10,12 @@ declare global {
     const route: typeof routeFn;
 }
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Salafy Borneo';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) =>
+        resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
