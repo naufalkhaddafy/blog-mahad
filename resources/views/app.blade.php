332b0690de6d81237ff50c483e2591632b73f6c9
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2025-03-20 22:26:50 +0800

    set: icon system

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Salafy Borneo') }}</title>
-
+    <link rel="icon" href="{{ asset('assets/icon.png') }}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
