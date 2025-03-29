284d0b522afb02cc64482ce5fabf5d5a398709f7
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2025-03-29 22:52:31 +0800

    feat: handle loading, buffering, and error radio play

--- resources/js/components/blog/RadioPlay.tsx
+++ resources/js/components/blog/RadioPlay.tsx
@@ -1,13 +1,15 @@
 import useRadio from '@/hooks/useRadio';
 import { router } from '@inertiajs/react';
-import { Pause, Play, Square } from 'lucide-react';
-import { useRef, useState } from 'react';
+import { LoaderCircle, Pause, Play, Square, X } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
 import { Container } from '../Container';
 
 export const RadioPlay = () => {
     const { channel, setChannelPlay } = useRadio();
     const audioRef = useRef<HTMLAudioElement | null>(null);
     const [pause, setPause] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errors, setErrors] = useState(false);
 
     const togglePlayPause = (value: string) => {
         if (audioRef.current && value == 'stop') {
@@ -28,62 +30,109 @@ export const RadioPlay = () => {
         }
     };
 
+    const handleLoading = () => {
+        setIsLoading(false);
+    };
+
+    const handleBuffering = () => {
+        setIsLoading(true);
+    };
+
+    const handleError = () => {
+        setErrors(true);
+    };
+    const handlePause = () => {
+        togglePlayPause('stop');
+        router.visit('/radio-online');
+        setChannelPlay();
+    };
+
+    useEffect(() => {
+        setIsLoading(true);
+        setErrors(false);
+    }, [channel]);
+
     return (
         <>
             {channel.url && (
                 <div className="bg-green-800">
                     <Container className="w-full max-w-2xl px-3 py-4 text-white">
-                        <div className="flex items-center justify-between gap-5">
-                            <div
-                                className="flex items-center gap-5"
-                                onClick={() => router.visit('/radio-online')}
-                            >
-                                <img
-                                    src={channel.image}
-                                    alt={channel.name}
-                                    className="aspect-square w-13 rounded-lg"
-                                    loading="lazy"
-                                />
-                                <div className="overflow-hidden">
-                                    <div className="flex items-center gap-3 py-2 font-bold text-ellipsis whitespace-nowrap">
-                                        {channel.name}
-                                        {channel.status === 'live' ? (
-                                            <span className="flex animate-pulse items-center rounded-2xl bg-red-700 px-2 py-1 text-xs text-white">
-                                                Live
-                                            </span>
-                                        ) : (
-                                            ''
-                                        )}
+                        {errors ? (
+                            <div className="flex items-center justify-between px-3 py-5">
+                                <p>Afwan radio tidak dapat diputar</p>
+                                <button onClick={handlePause}>
+                                    <X className="cursor-pointer transition duration-300 hover:scale-130 hover:text-red-500 active:text-red-500" />
+                                </button>
+                            </div>
+                        ) : (
+                            <div className="flex items-center justify-between gap-5">
+                                <div
+                                    className="flex items-center gap-5"
+                                    onClick={() => router.visit('/radio-online')}
+                                >
+                                    <img
+                                        src={channel.image}
+                                        alt={channel.name}
+                                        className="aspect-square w-13 rounded-lg"
+                                        loading="lazy"
+                                    />
+                                    <div className="overflow-hidden">
+                                        <div className="flex items-center gap-3 py-2 font-bold text-ellipsis whitespace-nowrap">
+                                            {channel.name}
+                                            {channel.status === 'live' ? (
+                                                <span className="flex animate-pulse items-center rounded-2xl bg-red-700 px-2 py-1 text-xs text-white">
+                                                    Live
+                                                </span>
+                                            ) : (
+                                                ''
+                                            )}
+                                        </div>
+                                        <marquee
+                                            behavior="scroll"
+                                            direction="left"
+                                            scrollamount="2"
+                                        >
+                                            {channel.stats?.description}
+                                        </marquee>
                                     </div>
-                                    <marquee behavior="scroll" direction="left" scrollamount="2">
-                                        {channel.stats?.description}
-                                    </marquee>
+                                </div>
+                                <div className="flex items-center gap-3">
+                                    <audio
+                                        ref={audioRef}
+                                        src={`${channel.url}/stream`}
+                                        autoPlay
+                                        onPlay={handleLoading}
+                                        onWaiting={handleBuffering}
+                                        onError={handleError}
+                                    />
+                                    <div>{errors && 'errror eg'}</div>
+                                    {isLoading ? (
+                                        <div className="px-5">
+                                            <LoaderCircle className="size-8 animate-spin" />
+                                        </div>
+                                    ) : (
+                                        <>
+                                            <button
+                                                onClick={() => togglePlayPause('pause')}
+                                                className="cursor-pointer rounded-full bg-green-700 p-2 transition hover:bg-green-500 active:bg-green-500"
+                                            >
+                                                {pause ? (
+                                                    <Pause className="size-4" />
+                                                ) : (
+                                                    <Play className="size-4" />
+                                                )}
+                                            </button>
+                                            <button
+                                                onClick={handlePause}
+                                                className="cursor-pointer rounded-full bg-green-700 p-2 transition hover:bg-green-500 active:bg-green-500"
+                                            >
+                                                <Square className="size-4" />
+                                            </button>
+                                        </>
+                                    )}
                                 </div>
                             </div>
-
-                            <div className="flex items-center gap-3">
-                                <audio ref={audioRef} src={`${channel.url}/stream`} autoPlay />
-                                <button
-                                    onClick={() => togglePlayPause('pause')}
-                                    className="cursor-pointer rounded-full bg-green-700 p-2 transition hover:bg-green-500 active:bg-green-500"
-                                >
-                                    {pause ? (
-                                        <Pause className="size-4" />
-                                    ) : (
-                                        <Play className="size-4" />
-                                    )}
-                                </button>
-                                <button
-                                    onClick={() => {
-                                        togglePlayPause('stop');
-                                        router.visit('/radio-online');
-                                    }}
-                                    className="cursor-pointer rounded-full bg-green-700 p-2 transition hover:bg-green-500 active:bg-green-500"
-                                >
-                                    <Square className="size-4" />
-                                </button>
-                            </div>
-                        </div>
+                        )}
                     </Container>
                 </div>
             )}
