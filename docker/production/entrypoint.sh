dd7a80d00004d266cbea991d67fe61377ecacb51
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2025-09-01 20:08:20 +0800

    fix: entry point

--- docker/production/entrypoint.sh
+++ docker/production/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
+chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Clear and rebuild caches every container start (uses env inside container)
 php artisan config:clear
