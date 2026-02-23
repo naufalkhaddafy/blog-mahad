710ff12b5e09afef611e996e6b97f09f1ba72656
Naufal Khaddafy <naufalkhaddafy136@gmail.com>
2026-02-23 22:33:11 +0800

    fix: bug position

--- routes/web.php
+++ routes/web.php
@@ -39,8 +39,8 @@ Route::get('/api/search', [BlogController::class, 'search'])->name('api.search')
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
     Route::resource('category', CategoryController::class)->except('show', 'edit', 'create');
-    Route::resource('posts', PostController::class);
     Route::post('/posts/autosave/{post?}', [PostController::class, 'autosave'])->name('posts.autosave');
+    Route::resource('posts', PostController::class);
     Route::resource('tag', TagController::class)->except('show', 'edit', 'create');
     Route::resource('banner', BannerController::class)->except('show', 'edit', 'create');
     Route::post('/banner/reorder', [BannerController::class, 'reorder'])->name('banner.reorder');
